fa718345f7b118fac48bede0c5ae4776da0f5111
David-Shibley <davidjshibley@gmail.com>
2016-02-14 11:41:46 -0700

    pushing to github

--- app.js
+++ app.js
@@ -1,21 +1,31 @@
 var express = require('express');
-var cowsay = require('cowsay');
+var cowsay  = require('cowsay');
+var yosay  = require('yosay');
 
 var app = express();
 
-var books = ["Javascript-The Good Parts", "Eloquent Javascript",
-"Horton Hears a Who"];
+var books = ['Javascript-The Good Parts', 'Eloquent Javascript',
+'Horton Hears a Who'];
 
-app.get('/cowsay/:cowsays', function(request, response){
-  response.setHeader('content-type','text/html');
+app.get('/cowsay/:cowsays', function(request, response) {
+  response.setHeader('content-type', 'text/html');
   response.send(
-    "<pre>" +
+    '<pre>' +
       cowsay.say({'text': request.params.cowsays}) +
-    "</pre>"
+    '</pre>'
   );
-})
+});
 
-app.get('/', function(request, response){
+app.get('/yosay/:yosays', function(request, response) {
+  response.setHeader('content-type', 'text/html');
+  response.send(
+    '<pre>' +
+      yosay({'text': request.params.yosays}) +
+    '</pre>'
+  );
+});
+
+app.get('/', function(request, response) {
   response.json(books);
 });
 
